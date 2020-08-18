10a72f1c33a2920d343932276a441a9f76e0b457
Roland Pihlakas <roland@simplify.ee>
2020-08-18 23:33:33 +0300

    Ensure that no deadlock may occur when two files are swapped

--- Program.cs
+++ Program.cs
@@ -551,8 +551,17 @@ namespace AsyncToSyncCodeRoundtripSynchroniserMonitor
                         //NB! if file is renamed to cs~ or resx~ then that means there will be yet another write to same file, so lets skip this event here
                         if (!rfse.FileSystemInfo.FullName.EndsWith("~"))
                         {
-                            using (await FileLocks.LockAsync(rfse.FileSystemInfo.FullName, token))
-                            using (await FileLocks.LockAsync(rfse.PreviousFileSystemInfo.FullName, token))
+                            var filenames = new List<string>()
+                            {
+                                rfse.FileSystemInfo.FullName,
+                                rfse.PreviousFileSystemInfo.FullName
+                            };
+
+                            //NB! in order to avoid deadlocks in case of file swaps, always take the locks in deterministic order
+                            filenames.Sort(StringComparer.InvariantCultureIgnoreCase);
+
+                            using (await FileLocks.LockAsync(filenames[0], token))
+                            using (await FileLocks.LockAsync(filenames[1], token))
                             {
                                 await FileUpdated(rfse.FileSystemInfo.FullName, context);
                                 await FileDeleted(rfse.PreviousFileSystemInfo.FullName, context);
